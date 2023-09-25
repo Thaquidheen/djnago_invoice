9d81a6cddc90944682e96c18b3ac289b7f8ec878
Thaquidheen M A <thaquidheen@gmail.com>
2023-09-25 17:28:45 +0530

    commit

--- invoice/invoice/settings.py
+++ invoice/invoice/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'additem',
     'customer',
     'order',
+    'orderitem',
 ]
 
 MIDDLEWARE = [
---
+++ invoice/orderitem/models.py
@@ -0,0 +1,16 @@
+
+
+from django.db import models
+from ..additem.models import item
+from invoice.order.models import Order
+
+
+class OrderItem(models.Model):
+   
+    Orderid_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    itemid_id = models.ForeignKey(item, on_delete=models.CASCADE)
+    Quantity = models.PositiveIntegerField()
+    Amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"OrderItem ID: {self.OrderItemID} - Order: {self.OrderID} - Item: {self.ItemID}"
